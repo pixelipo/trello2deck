7d115542aac0875271864152ab782633b97a9ac5
Marin Treselj <marin.treselj@forlagshuset.no>
2021-03-31 16:50:49 +0200

    Clean up main.py

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 from app import trello, parser, deck, db
 
 
-service = input("Select service([1] Fetch from Trello [2] Fetch from JSON [3] Export to Deck:")
+service = input("Select service:\n[1] Import from Trello\n[2] Import from JSON\n[3] Export to Deck\n")
 
-## Connect to Trello via API
+## Import data from Trello to SQLite Database
 if (service == '1'):
-    # baseUrl = 'https://api.trello.com/1/members/me/boards?'
-    # res = trello.createBoards(trello.connect(baseUrl))
-    # if res:
-        # print(res)
-    # print(trello.createLists())
-    # print(trello.createCards())
-    conn = db.initDb('data/db.sqlite3')
-    cur=conn.cursor()
-    print(db.getCards(cur).fetchall())
+    baseUrl = 'https://api.trello.com/1/members/me/boards?'
+    trello.createBoards(trello.connect(baseUrl))
+    print(trello.createLists())
+    print(trello.createCards())
 
 if (service == '2'):
     data = parser.parseJson(input("type filename (default: data/trello.json):"))
